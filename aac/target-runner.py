74f1c562da0d1090995b7c62c60494e4fb097ee9
Marcelo de Souza <bsi.marcelo@gmail.com>
2021-01-08 16:07:54 -0300

    Update output redirection.

--- aac/target-runner.py
+++ aac/target-runner.py
@@ -32,8 +32,8 @@ os.unlink(directory + '/algorithm.cpp')
 shutil.move(algorithm, directory + '/algorithm.cpp')
 
 # Compile the generated algorithm into the solver
-os.system('cd ' + directory + '; cmake . >> /dev/null')
-os.system('cd ' + directory + '; make runner >> /dev/null')
+os.system('cd ' + directory + '; cmake . 1> /dev/null 2> /dev/null')
+os.system('cd ' + directory + '; make runner 1> /dev/null 2> /dev/null')
 
 # Create execution command
 exe = directory + '/runner'
@@ -68,4 +68,63 @@ print(result)
 os.remove(out_file)
 os.remove(err_file)
 shutil.rmtree(directory)
+sys.exit(0)
+
+
+#!/usr/bin/python3
+import os
+import sys
+import subprocess
+
+if len(sys.argv) < 5:
+    print("\nUsage: ./target-runner.py <configuration_id> <instance_id> <seed> <instance_path_name> <list of parameters>\n")
+    sys.exit(1)
+
+# Get the parameters as command line arguments.
+configuration_id = sys.argv[1]
+instance_id = sys.argv[2]
+seed = sys.argv[3]
+instance = sys.argv[4]
+conf_params = ' '.join(sys.argv[5:])
+
+# Create execution command
+fixed_params = '-r 1 --quiet -t 10'
+command = './acotsp ' + fixed_params + ' -i ' + instance + ' --seed ' + seed + ' ' + conf_params
+
+
+# Define the stdout and stderr files.
+out_file = './c' + str(configuration_id) + '-' + str(instance_id) + str(seed) + '.stdout'
+err_file = './c' + str(configuration_id) + '-' + str(instance_id) + str(seed) + '.stderr'
+
+# Execute
+outf = open(out_file, "w")
+errf = open(err_file, "w")
+return_code = subprocess.call(command, stdout = outf, stderr = errf, shell = True)
+outf.close()
+errf.close()
+
+# Check return code
+if return_code != 0:
+    print('Command returned code ' + str(return_code) + '.')
+    sys.exit(1)
+
+# Check output file
+if not os.path.isfile(out_file):
+    print('Output file <' + out_file  + '> not found.')
+    sys.exit(1)
+    os.remove(out_file)
+    os.remove(err_file)
+    sys.exit(0)
+
+# Check if an error occured
+if os.stat(out_file).st_size != 0:
+    print('Inf')
+        
+# Get result and print it
+result = open(out_file).read().split()[-1].split(';')[1]
+print(result)
+
+# Clean files and exit
+os.remove(out_file)
+os.remove(err_file)
 sys.exit(0)
